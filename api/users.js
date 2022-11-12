26ee51fe994ab1809d6806ae07fcf9a9f4968932
skhacker105 <skhacker105@gmail.com>
2022-11-12 17:33:03 +0530

    updated query param

--- api/users.js
+++ api/users.js
@@ -5,15 +5,19 @@ const skops_mongo = require('../db/mongodb');
 router.get('/', (req, res) => {
     const email = req.query.email;
     const pass = req.query.pass;
-    skops_mongo.getDb().collection('users').find({
-        email: email,
-        password: pass
-    }).toArray((err, result) => {
-        if (err) res.send(err)
-        if (result) {
-            res.json(result);
-        }
-    });
-    
+    if (skops_mongo.getDb()) {
+        skops_mongo.getDb().collection('users').find({
+            email: email,
+            password: pass
+        }).toArray((err, result) => {
+            if (err) res.send(err)
+            if (result) {
+                res.json(result);
+            }
+        });
+    } else {
+        res.send('No DB')
+    }
+
 });
 module.exports = router;
