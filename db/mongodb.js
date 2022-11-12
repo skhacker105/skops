37c39f39775405a5459989c90e2064f690f07d55
skhacker105 <skhacker105@gmail.com>
2022-11-12 19:54:11 +0530

    minor update

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,54 +1,20 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient } = require('mongodb');
 var username = encodeURIComponent("skhacker105");
 var password = encodeURIComponent("Sk8886161092#");
 const uri = `mongodb+srv://${username}:${password}@cluster0.edzzmnk.mongodb.net/?retryWrites=true&w=majority`;
-const _db = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// var _db;
+
+var _db;
 
 module.exports = {
-    dbConnectionTest: function (res) {
-        client.connect(err => {
-            if (err) res.json({ error: 'Connection Error - ' + err })
-            const users = client.db("skops").collection("users");
-            // perform actions on the collection object
-            // users.find({
 
-            // })
-            client.close();
-            res.send('DB Connection Successfull');
+    connectToServer: function (callback) {
+        MongoClient.connect(uri, function (err, client) {
+            _db = client.db('skops');
+            return callback(err);
         });
+    },
+
+    getDb: function () {
+        return _db;
     }
 };
-
-// module.exports = {
-
-//     connectToServer: function (callback) {
-//         try {
-//             MongoClient.connect(uri, function (err, client) {
-//                 _db = client.db('skops');
-//                 return callback(err);
-//             });
-//         } catch(ex) {
-//             return callback(ex);
-//         }
-//     },
-
-//     reconnectToServer: function(res) {
-//         console.log('Reconnecting to DB Server using -- ', uri);
-//         try {
-//             MongoClient.connect(uri, function (err, client) {
-//                 _db = client.db('skops');
-//                 res.json({
-//                     err: err,
-//                     client, client
-//                 });
-//             });
-//         } catch(ex) {
-//             res.send(ex);
-//         }
-//     },
-
-//     getDb: function () {
-//         return _db;
-//     }
-// };
