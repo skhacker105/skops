a9b62532696c9154e8e8b3786a2c50031ef8de37
skhacker105 <skhacker105@gmail.com>
2022-11-13 13:55:18 +0530

    minor update

--- helper.js
+++ helper.js
@@ -1,5 +1,5 @@
 module.exports = {
     getToken: function(req) {
-        return req.body.token || req.query.token || req.headers["x-access-token"] || (req.headers["authorization"].replace('Bearer ', ''))
+        return req.body.token || req.query.token || req.headers["x-access-token"] || (req.headers["authorization"] ? req.headers["authorization"].replace('Bearer ', '') : '')
     }
 }
