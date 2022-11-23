f9530b501890b62556bfc07d704bbc4a09b5e527
skhacker105 <skhacker105@gmail.com>
2022-11-24 01:33:52 +0530

    handled error structure

--- api/users.js
+++ api/users.js
@@ -89,7 +89,7 @@ router.post('/login', (req, res) => {
             }
         });
     } catch (err) {
-        res.status(500).send(err);
+        res.status(500).json({ message: err });
     }
 
 });
